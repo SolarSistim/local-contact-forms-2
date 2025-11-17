24dbe9f395fc9c2fbaac1fc151a638948d81a5d8
SolarSistim <47832728+SolarSistim@users.noreply.github.com>
2025-11-17 16:50:26 -0600

    Added placeholder homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,46 @@
 import { Routes } from '@angular/router';
 import { ContactForm } from './components/contact-form/contact-form';
+import { PageHome } from './components/pages/page-home/page-home';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+// Create a resolver component
+@Component({
+  selector: 'app-route-resolver',
+  standalone: true,
+  template: ''
+})
+export class RouteResolverComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      if (params['id']) {
+        // Has id parameter, navigate to contact form with params
+        this.router.navigate(['/contact'], { queryParams: params });
+      } else {
+        // No id parameter, go to home
+        this.router.navigate(['/home']);
+      }
+    });
+  }
+}
 
 export const routes: Routes = [
   {
     path: '',
-    component: ContactForm,
+    component: RouteResolverComponent,
     pathMatch: 'full'
+  },
+  {
+    path: 'contact',
+    component: ContactForm
+  },
+  {
+    path: 'home',
+    component: PageHome
   }
 ];
